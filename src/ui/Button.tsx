11b362f3490aed747ae26ab5b24727e1c2c884f7
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-09-27 19:41:39 -0300

    feat - added new variations to button component

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,10 +1,27 @@
-import { PencilSimpleLine } from "@phosphor-icons/react";
+import { twMerge } from "tailwind-merge";
 
-export function Button() {
+interface ButtonProps {
+	icon?: React.ReactElement;
+	text: string;
+	variation: "outlined" | "filled";
+}
+
+const Variations = {
+	outlined:
+		"border border-clr-green-100 text-clr-green-200 bg-transparent hover:bg-clr-green-200 hover:text-clr-white ",
+	filled: "bg-clr-green-200 text-clr-white hover:bg-clr-green-100",
+};
+
+export function Button({ text, icon, variation }: ButtonProps) {
 	return (
-		<button className="flex items-center justify-center px-6 pt-4 pb-3.5 gap-2 rounded-lg border border-clr-green-100 text-clr-green-200 font-bold text-base bg-transparent hover:bg-clr-green-200 hover:text-clr-white transition-colors duration-100">
-			<PencilSimpleLine size={20} />
-			Editar seu perfil
+		<button
+			className={twMerge(
+				"flex items-center justify-center px-6 pt-4 pb-3.5 gap-2 rounded-lg text-base transition-colors duration-100 font-bold",
+				Variations[variation]
+			)}
+		>
+			{icon}
+			{text}
 		</button>
 	);
 }
